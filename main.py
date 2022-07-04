df6c18a2a08eabb46f135de41f8e3a0345b4a565
lavender010101 <lavender010101@163.com>
2022-07-04 17:50:09 +0800

    sensor

--- main.py
+++ main.py
@@ -33,14 +33,14 @@ if __name__ == '__main__':
     try:
         while True:
             if sensor.check_front() == 'forward':
-                car.forward(40, 0)
+                car.forward(30, 0)
             elif sensor.check_front() == 'stop':
                 car.stop(0)
             elif sensor.check_front() == 'turn_left':
-                car.turn_left(30, 0)
+                car.turn_left(25, 0)
             elif sensor.check_front() == 'turn_right':
-                car.turn_right(30, 0)
-            time.sleep(0.13)
+                car.turn_right(25, 0)
+            time.sleep(0.09)
 
             os.system('clear')
 
