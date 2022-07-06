17b2c2b6d384149220f69be63d16d567671b3185
lavender010101 <lavender010101@163.com>
2022-07-06 15:51:57 +0800

    servo test

--- PCA9685.py
+++ PCA9685.py
@@ -88,12 +88,14 @@ if __name__ == '__main__':
 
     pwm = PCA9685(0x40, debug=False)
     pwm.setPWMFreq(50)
+    pwm.setServoPulse(12, 1500)
+    time.sleep(0.5)
     while True:
         # setServoPulse(2,2500)
         for i in range(500, 2500, 10):
-            pwm.setServoPulse(0, i)
+            pwm.setServoPulse(12, i)
             time.sleep(0.02)
 
         for i in range(2500, 500, -10):
-            pwm.setServoPulse(0, i)
+            pwm.setServoPulse(12, i)
             time.sleep(0.02)
